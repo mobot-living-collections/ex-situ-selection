ace673a1984867fd481ee88576022c7868e1f83e
mobot-living-collections <taubuchon@mobot.org>
2024-09-17 10:25:49 -0500

    Update SurvivalDataPreparation_2024July03.R
    cleaned up homework sections and setwd sections with local details

--- SurvivalDataPreparation_2024July03.R
+++ SurvivalDataPreparation_2024July03.R
@@ -36,18 +36,6 @@
 ######################################################################################################
 
 #set working directory
-#setwd("C:/_transfer/Papers/OpportunityForSelection/Data") #Ivan's directory
-
-#################
-#Homework: set your working directory below and please don't erase mine above. We will be sharing versions
-#of this script and it is convenient not having to retype the working directory. Also, having the working
-#directories in the code, as oppossed to using the "file.choose" function, bypasses the need to manually
-#navigate to a directory. That will save time, given we will run each script many times and open and
-#save several files each time.    
-#################
-#setwd("") #Lauren's directory
-
-"C:/Users/laurd/OneDrive/Documents/MBG REU Internship/REU R Environment/REU Project"
 
 #read and examine survival data
 Qa.surv.data <- read.table(file="QaSurvivalData20230922.csv", header=TRUE, sep=",")
@@ -55,7 +43,6 @@ class(Qa.surv.data)
 dim(Qa.surv.data)
 head(Qa.surv.data)
 summary(Qa.surv.data)
-
 
 ######################################################################################################
 ## 2. Edit the data frame "Qa.surv.data" and create a new data frame named "Qa.sd".
@@ -76,17 +63,18 @@ Qa.sd <- Qa.sd[o.AccessionPlanting,]
 head(Qa.sd, n=20)
 tail(Qa.sd, n=20)
 summary(Qa.sd)
+
 #################
-#Homework: write annotations below, describing each of the variables (i.e., columns) in the
+#annotations below, describing each of the variables (i.e., columns) in the
 #data frame Qa.sd.   
 #################
 
 #Accession number represents a single mother plant.
 #Plantings are siblings of the same mother.
 #Position is the number used to randomize the position of a plant in the garden.
-#SurverDataPossiblyDead is the date that the plant was observed as likely being dead.
+#SurveyDataPossiblyDead is the date that the plant was observed as likely being dead.
 #PossiblyDead is whether or not the plant was observed as possibly dead.
-#SurveryDateDead is the data that plant death was confirmed.
+#SurveyDateDead is the data that plant death was confirmed.
 #Dead is whether or not the plant was actually dead.
 
 ######################################################################################################
@@ -103,14 +91,12 @@ par(mar=c(5, 4.5, 4, 2) + 0.1)
 barplot(table(Qa.sd$SurveyDatePossiblyDead), col="gray70", xlab="Date", ylab="Plantings", cex.lab=1.5, cex.axis=1.5, cex.names=1.5)
 barplot(PossiblyDead.per.date, add=T, density=15, col="black", axes=F, axisnames=F)
 #################
-#Homework: add a legend to the barplot above. Read the help page for function "legend".
-
+#add a legend to the barplot above
 legend("topright", c("Alive", "Possibly Dead"), col=c("gray70", "gray70"), fill=c("gray70", "black"), cex=1.1, density=c(NA,15))
 #################
 
 #there is only one date for the second survey:
 unique(Qa.sd$SurveyDateDead)
-
 
 ######################################################################################################
 ## 4. Compare survival data between the two surveys.
@@ -118,7 +104,6 @@ unique(Qa.sd$SurveyDateDead)
 
 #these rows have different survival data in the two surveys
 Qa.sd[Qa.sd$PossiblyDead != Qa.sd$Dead,]
-
 
 ######################################################################################################
 ## 5. Examine the distribution of plantings among accessions.
@@ -134,13 +119,7 @@ table(Qa.sd$AccessionNumber)
 sort(table(Qa.sd$AccessionNumber), decreasing=T)
 
 #graph the distribution of plantings per accession
-#################
-#Homework: modify the code below to edit the barplot and produce a high quality figure for
-#publication in Conservation Biology. Read the journal author guidelines regarding figures and see
-#examples in published papers. Focus on features of the barplot (including axis labels, axis annotation,
-#font size, etc.) as it appears in the graphical window. Do not worry for now about saving the figure
-#in a high resolution file. Read the help pages for functions "barplot" and "par".   
-#################
+
 barplot(sort(table(Qa.sd$AccessionNumber), decreasing=T), las=3) 
 
 barplot(sort(table(Qa.sd$AccessionNumber), decreasing=T), las=3, ylim=c(0, 11), ylab="Plantings", xlab="Accession Number")
@@ -173,26 +152,14 @@ identical(names(table(Qa.sd$AccessionNumber)[o.FrequencyPlantings]), names(death
 barplot(deaths.per.accession[o.FrequencyPlantings], las=3)
 
 #graph the distribution of deaths among accessions on top of the number of plantings per accession
-#################
-#Homework: modify the code below to edit the barplot and produce a high quality figure for
-#publication in Conservation Biology. See indications for previous homework. Again, read the help pages
-#for functions "barplot" and "par".
-#################
+
 barplot(table(Qa.sd$AccessionNumber)[o.FrequencyPlantings], las=3, col="transparent")
 barplot(deaths.per.accession[o.FrequencyPlantings], add=T, col="gray60", axisnames=F)
-
 
 ######################################################################################################
 ## 7. Save the data frame named "Qa.sd" as a text file.
 ######################################################################################################
 
-#set working directory
-#setwd("C:/_transfer/Papers/OpportunityForSelection/Data") #Ivan's directory
-#################
-#Homework: set your working directory below and don't erase mine above. We will be sharing future versions
-#of this script and it is convenient not having to retype the working directories.   
-#################
-#setwd("") #Lauren
 write.csv(Qa.sd, file=paste("QaSurDat_", format(Sys.time(), "%Y%b%d_%H%M%S"), ".csv", sep=""), row.names = F)
 
 
